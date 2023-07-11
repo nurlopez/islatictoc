e0c1e86d7b36e397d514c0f359d6452e7e67719c
nurlopez <nlopezcolom@gmail.com>
2023-07-11 12:39:45 +0200

    added empty article array conditional

--- islatictoc/src/app/page.tsx
+++ islatictoc/src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
         <div className='container mx-auto'>
           <div className='flex flex-col justify-between items-center text-center mb-16 text-white'>
             <Image
-            className='invert'
+              className='invert'
               src='/islatictoc_logo_text.png'
               alt='Isla Tic Toc Logo'
               width={300}
@@ -64,33 +64,41 @@ export default function Home() {
             </p>
           </div>
 
-          <section>
-            <div className='grid sm:grid-cols-2 gap-4 grid-cols-1 '>
-              <img
-                className='h-16 bg-white m-4 shadow-lg  bg-slate-700 border-b-2 border-yellow-300 py-3, px-4'
-                src='https://cdn.the-scientist.com/assets/articleNo/67560/iImg/37910/uploads/logo%20small.png'
-              />
-            </div>
+          {articles.length ? (
+            <section>
+              <div className='grid sm:grid-cols-2 gap-4 grid-cols-1 '>
+                <img
+                  className='h-16 bg-white m-4 shadow-lg  bg-slate-700 border-b-2 border-yellow-300 py-3, px-4'
+                  src='https://cdn.the-scientist.com/assets/articleNo/67560/iImg/37910/uploads/logo%20small.png'
+                />
+              </div>
 
-            <div className='grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-              {articles.map((item, index) => {
-                return (
-                  <Feed
-                    key={index}
-                    title={item.title}
-                    date={item.pubDate}
-                    author={item.creator}
-                    link={item.link}
-                    description={item.contentSnippet}
-                  />
-                );
-              })}
-            </div>
-          </section>
+              <div className='grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {articles.map((item, index) => {
+                  return (
+                    <Feed
+                      key={index}
+                      title={item.title}
+                      date={item.pubDate}
+                      author={item.creator}
+                      link={item.link}
+                      description={item.contentSnippet}
+                    />
+                  );
+                })}
+              </div>
+            </section>
+          ) : (
+            <section>
+              <p className='text-white'>
+                No hay artículos disponibles ahora mismo
+              </p>
+            </section>
+          )}
         </div>
       </main>
 
-      <footer className='footer footer-center w-full p-2 bg-gray-300 text-gray-800 bottom-0'>
+      <footer className='footer footer-center lg:absolute w-full p-2 bg-gray-300 text-gray-800 bottom-0'>
         <div className='text-center'>
           <p>
             {'© 2023' + ' '}
